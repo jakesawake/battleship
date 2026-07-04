d957d00a000ef473b4a17f946618bd27f59aa13c
Jake Canlas <jakechristiancanlas1@gmail.com>
2026-07-03 21:18:23 -0400

    Created game-logic module
    - for handling game loop (handle turn switching, attacks, hit/miss
    display, win condition)

---
+++ src/game-logic.js
@@ -0,0 +1,6 @@
+// TODO: TLDR: handle turn switching, attacks, hit/miss display, win condition
+// 1. display blank gameboard after the user places all their ships
+
+function gameLoop(humanPlayer, computerPlayer) {}
+
+export { gameLoop };
