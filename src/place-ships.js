d7e53ea8f1d52285ac84c5dfbf03c23c58f0e81c
Jake Canlas <jakechristiancanlas1@gmail.com>
2026-07-03 21:21:22 -0400

    Added data transfer logic for placing ships

--- src/place-ships.js
+++ src/place-ships.js
@@ -13,10 +13,6 @@ async function loadSVGElement(filepath) {
   return svgElement;
 }
 
-// async function to place svg's of the ships for the humanPlayer
-// passing in the humanPlayer object from main.js
-// this module will then have access to both humanPlayer.ships &
-// humanPlayer.gameboard (to call placeShip on when a ship is dropped)
 async function placeShips(humanPlayer) {
   let loadedSVG;
 
@@ -26,9 +22,11 @@ async function placeShips(humanPlayer) {
   for (const ship of humanPlayer.ships) {
     let filePathStr = `/${ship.name}.svg`;
     loadedSVG = await loadSVGElement(filePathStr);
+
     const shipContainer = document.createElement("div");
     shipContainer.classList.add("ship-container");
     shipContainer.setAttribute("draggable", "true");
+    shipContainer.dataset.shipName = `${ship.name}`;
 
     shipContainer.addEventListener("click", () => {
       ship.toggleOrientation();
@@ -39,14 +37,6 @@ async function placeShips(humanPlayer) {
       }
     });
 
-    // - adding event listener on the ship container to listen to dragstart events
-    // - (fired on the source element): this event triggers the exact moment a user
-    // clicks and beings dragging the item
-    // NOTE: dataTransfer object holds the data that is being dragged during a drag-and-drop operation or transferred via clipboard
-    // allows you to pass specific data like text or files from the element you are dragging to the element you are dropping to
-    // setData() stores data during the drag operation, you define the data's format ie. text/plain,
-    // and then the actual content, in this case passing in the ship's named and orientation as an object so we can pass in multiple values to dataTransfer
-    // from the drop side we can destructure the object we've sent in to retrieve the data
     shipContainer.addEventListener("dragstart", (e) => {
       e.dataTransfer.setData(
         "text/plain",
@@ -58,7 +48,6 @@ async function placeShips(humanPlayer) {
   }
   gameAreaContainer.appendChild(shipOuterContainer);
   container.appendChild(gameAreaContainer);
-  // TODO: hide the shipOuterContainer after putting the ships on the board
 }
 
 export { placeShips };
