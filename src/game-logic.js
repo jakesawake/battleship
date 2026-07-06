07165231e3a68379bb5482e1a85d3526e0aa6d89
Jake Canlas <jakechristiancanlas1@gmail.com>
2026-07-06 19:05:51 -0400

    Removed TODO's

--- src/game-logic.js
+++ src/game-logic.js
@@ -1,7 +1,3 @@
-// TODO: TLDR: handle turn switching, attacks, hit/miss display, win condition
-// [] call playerAttack method from Player class & pass in the players gameboard and attack coordinate
-// NOTE: Player objects pretty much have everything including each players gameboard and their attacks/misses
-
 function gameLoop(callback, humanPlayer, computerPlayer) {
   callback(humanPlayer, computerPlayer);
 }
